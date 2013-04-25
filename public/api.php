76ef3635cea82df71191568b1d80235116a73714
root <root@ubuntu.(none)>
2013-04-25 19:13:36 +0800

    add jquery datatalbes ui

--- public/api.php
+++ public/api.php
@@ -2,6 +2,18 @@
 require_once 'memcache_array.php';
 require_once 'config.php';
 
+$http_referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null;
+$referer = ($http_referer)? parse_url($http_referer) : null;
+header('Access-Control-Allow-Origin: '.($referer ? ($referer['scheme'].'://'.$referer['host']) : '*'));
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Credentials: true');
+
+if (isset($_GET['cmd']) or isset($_POST['cmd'])) goto label_api_mode;
+
+
+exit();
+
+label_api_mode:
 # 获取设备id和分类和发送
 # get http://omp.cn/api.php?cmd=list&type=[device|browser|platform|mobile]
 # post http://omp.cn/api.php?cmd=send&type=[device|browser|platform|mobile]&value=xxxxx
@@ -10,5 +22,7 @@ require_once 'config.php';
 # get http://omp.cn/api.php?cmd=listplats
 # get http://omp.cn/api.php?cmd=listrole&plat=tencent_qq
 # post http://omp.cn/api.php?cmd=sendrole&plat=tencent_qq&username=xxxx&nickname=xxxx
+
+
 
 ?>
